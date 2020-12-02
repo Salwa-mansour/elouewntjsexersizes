0a321e2e18b896d69b3cb751030ab9cd4c67d258
Salwa-Mansour <alqadasisal@gmail.com>
2020-12-02 23:08:17 +0300

    the scond exersize solved

--- page37-1stex.js
+++ page37-1stex.js
@@ -1,9 +1,43 @@
 
-console.log("hello");
-for(i=0;i<8;i++){
-    for(j=0;i<5;i++){
-        console.log("*");
+// console.log("hello");
+// firest exersize 
+// let line;
+// for(line="#";line.length<8;line+="#")
+// {
+//     console.log(line);
+// }
+
+// let num ;
+// for(num=1 ;num < 101;num++){
+//     if( num % 3 == 0 && num %5 == 0)
+//    console.log("Fizzbizz");
+//    else if (num %3 == 0)
+//    console.log("Fizz");
+//    else if (num %5 == 0)
+//    console.log("Bizz");
+//    else
+//    console.log(num);
+// }
+
+// for (let n = 1; n <= 100; n++) {
+//     let output = "";
+//     if (n % 3 == 0) output += "Fizz";
+//     if (n % 5 == 0) output += "Buzz";
+//     console.log(output || n);
+  
+//   }
+let chess="";
+for(let i=0; i<=8 ;i++){
+    for(let j=0 ; j<=8 ; j++){
+        if(i%2!=0){
+            chess+=" #"
+        }
+        if(i%2==0){
+            chess+="# "
+        }
+        
     }
+    // console.log("\n")
+    console.log(chess)
+    chess="";
 }
-for (let line = "#"; line.length < 8; line += "#")
-  console.log(line);

f18520169663adfe42965cd27101698f66c874d5
Salwa-Mansour <alqadasisal@gmail.com>
2020-12-07 22:58:18 +0300

    p

---
+++ functions.js
@@ -0,0 +1,16 @@
+const less=function(x,y){
+    if(x<y)
+    console.log(x);
+    else if (x == y)
+    console.log("Equals")
+    else
+    console.log(y)
+}
+//  less(12,24);
+ const lessthan=function(x,y){
+     if(x<y)
+     return x;
+     else
+     return y;
+ }
+console.log(lessthan(6,4));

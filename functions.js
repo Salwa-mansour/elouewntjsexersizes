e068083ad0ad0b70c4a345afafba14312a880b27
Salwa-Mansour <alqadasisal@gmail.com>
2021-01-07 11:00:15 +0300

    back to javascribt

--- functions.js
+++ functions.js
@@ -1,16 +1,46 @@
-const less=function(x,y){
+  function less(x,y){
     if(x<y)
-    console.log(x);
+    return x;
+  
     else if (x == y)
-    console.log("Equals")
+    return "Equals";
     else
-    console.log(y)
+    return y;
 }
-//  less(12,24);
+ less(12,24);
  const lessthan=function(x,y){
      if(x<y)
      return x;
      else
      return y;
  }
-console.log(lessthan(6,4));
+//console.log(less(6,8));
+
+////////// Bean counting exircise page 56 /////////////
+function countBs(bString){
+    let count=0;
+      for(i=0;i<=bString.length-1;i++){
+        if(bString[i] == "B"){
+            count+=1;
+
+        }
+      }
+      return count;
+}
+
+//console.log(countBs('BddddddddB'));
+
+////////////// letter counting same exircise //////////
+
+function countLetters(bString,letter){
+    let count=0;
+      for(i=0;i<=bString.length-1;i++){
+        if(bString[i] == letter){
+            count+=1;
+
+        }
+      }
+      return count;
+}
+
+console.log(countLetters('BddddddddB','d'));

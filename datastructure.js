a9870b6e182eb3ee342e3987f04e77788ade67e5
Salwa-Mansour <alqadasisal@gmail.com>
2021-01-08 16:33:58 +0300

    javascript aseincials

---
+++ datastructure.js
@@ -0,0 +1,19 @@
+// defingin arry
+let listOfNumbers = [2,3,4,5,66];
+// console.log(listOfNumbers[0]);
+let alShaibaniGlads = ['networking','analiysis and desigen','opraiting systems','desingen','technews','computer hacks','typing','hardware software maintnainace'];
+
+function goodEng(){
+for(i=0;i<alShaibaniGlads.length;i++){
+    console.log(alShaibaniGlads[i]);
+}
+}
+//arry elements console loging
+//goodEng();
+alShaibaniGlads.push('good teacher');
+////arry structer consol loging
+//console.log(alShaibaniGlads);
+//consol log the returned value
+ //console.log(alShaibaniGlads.pop());
+ alShaibaniGlads.pop();
+goodEng();

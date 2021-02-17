5f77562c48ad328d609819340c998cc17a874108
Salwa-Mansour <alqadasisal@gmail.com>
2021-02-17 22:12:14 +0300

    arry to object

--- datastructure.js
+++ datastructure.js
@@ -1,5 +1,5 @@
 // page 79 exersizes 
-//1.1
+////////////////////1.1----------------------------------------------------
 
 function myRange(x,y,step=1){
  var rangeArr=[];
@@ -21,7 +21,7 @@ if( range<0){
 }
  return rangeArr;
 }
-console.log(myRange(15,5,2));
+// console.log(myRange(15,5,2));
 // console.log(sum(range(1,10)))
 function arrSum(arr){
   sum = 0;
@@ -32,4 +32,67 @@ function arrSum(arr){
   return sum;
 }
 a=[1,2,3,4,5,6,7,8,9,10];
-console.log(arrSum(a));
+b=[1,2,3,4,5,6,7,8,9];
+// console.log(a.length);
+// console.log(arrSum(a));
+
+/////////////////// 2nd exersize----------------------------------------------
+
+function revArr1(arr){
+
+ let r=[];
+  let len= arr.length;
+  for(i=0;i<len;i++){
+  
+     r.push(arr.pop());
+  }
+
+  return r ;
+}
+// console.log(revArr1(a));
+///////
+function revArr2(arr){
+  let x;
+  for(i=1 ;i< arr.length;i++){
+    // for(k= Math.floor(arr.length/2);k>0;k--){
+      x= arr[i];
+      console.log(arr.length)
+      console.log(i)
+      arr[i]=arr[arr.length-2];
+      arr[arr.length-i]=x;
+    }
+  // }
+  return arr;
+}
+// console.log(revArr2(a));
+// problem not sovled
+function reverseArrayInPlace(arr){
+  let middle= Math.floor(arr.length / 2);
+  let x;
+  for(i=0;i<middle;i++){
+    x=arr[i];
+    arr.shift();
+    arr.unshift(arr.pop());
+    arr.push(x);
+  }
+  return arr;
+}
+// console.log(reverseArrayInPlace(b)) ;
+/////////////////// 3nd exersize----------------------------------------------
+///////3.0
+let c=['dogs','cats','chikens']
+
+function arryToList(arry){
+   opj={};
+   for(i=0;i<arry.length;i++){
+   opj[i]=arry[i];
+  }
+  return opj;
+}
+console.log(arryToList(c));
+let myL
+ist =arryToList(c);
+///////////////////3.1
+function addToList(list,elemnt){
+
+}
